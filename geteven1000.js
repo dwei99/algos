714692cbb6463a84fba668b5c9a5c2802b6fea3f
Dwei99 <alexanderanderson1@gmail.com>
2016-10-18 11:31:31 -0500

    Algorithm to sum up all even numbers from 1 to 1000

---
+++ geteven1000.js
@@ -0,0 +1,16 @@
+
+/*
+Write a function that would get the sum of all the even numbers from 1 to 1000.
+ You may use a modulus operator for this exercise. */
+function sum_even_numbers(){
+    var sum = 0;
+    for(var i = 0; i<=1000; i++)
+    {
+        if(i%2 === 0)
+        {
+        sum += i;
+        }
+    }
+    return sum;
+}
+console.log(sum_even_numbers());
