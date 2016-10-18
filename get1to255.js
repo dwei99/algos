14438dd98994c0da8d612f5a51831be9048e6e28
Dwei99 <alexanderanderson1@gmail.com>
2016-10-18 15:18:34 -0500

    updated logic for algorithms

--- get1to255.js
+++ get1to255.js
@@ -1,3 +1,6 @@
+/*
+Write a function that returns an array with all the numbers from 1 to 255.
+*/
 function get_array() {
     var arr = [];
     for(var i =1; i<=255; i++) {
--- geteven1000.js
+++ geteven1000.js
@@ -1,7 +1,7 @@
 
 /*
 Write a function that would get the sum of all the even numbers from 1 to 1000.
- You may use a modulus operator for this exercise. */
+ */
 function sum_even_numbers(){
     var sum = 0;
     for(var i = 0; i<=1000; i++)
