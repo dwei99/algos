2e8f531f5a75b6d683fc60865917bc7607b3ad2f
Dwei99 <alexanderanderson1@gmail.com>
2016-10-18 11:32:09 -0500

    added comments

--- find_average.js
+++ find_average.js
@@ -1,4 +1,8 @@
-var arr = [1,2,3,4,5];
+/*
+Given an array with multiple values, write a function that returns the average
+of the values in the array. (e.g. for [1,3,5,7,20] average is 7.2)
+*/
+var arr = [1,3,5,7,20];
 function findAvg(arr) {
     //your code here
     var avg = 0;
